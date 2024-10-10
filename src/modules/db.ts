4e7c84d35895f9296865cf997e5323e3934ad8e4
Ushakov Igor <80533977+MrGodOfNight@users.noreply.github.com>
2024-10-10 10:22:59 +0500

    Update db.ts

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: '81.31.247.100',
-  port: 3306,
-  user: 'EPlTfT',
-  password: 'UlYMWPeFeQtnDsMM',
-  database: 'jArbWOwo',
+  host: '',
+  port: ,
+  user: '',
+  password: '',
+  database: '',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
